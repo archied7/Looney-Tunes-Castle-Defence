b7952407117fd5a05814313cc166b4d6fe1c221f
Archie <archiedolman123@gmail.com>
2024-11-11 11:47:30 +0000

    added state class

--- game/classes/states/stateClass.py
+++ game/classes/states/stateClass.py
@@ -2,6 +2,7 @@ class State():
     def __init__(self, main) -> None:
         self.main = main
         self.previousState = None
+        self.loadImages()
 
     #abstract update function
     def update(self) -> None:
@@ -9,6 +10,10 @@ class State():
 
     #abstract render function
     def render(self) -> None:
+        pass
+
+    #abstract function to load images for the state
+    def loadImages(self) -> None:
         pass
 
     def newState(self) -> None:
--- game/main.py
+++ game/main.py
@@ -1,47 +1,45 @@
 import pygame
 import time
 import os
-from classes import gameClass, buttonClass, stateClass
+from classes.states import mainMenuState
 
 class Main():
     #initialise Main class
     def __init__(self) -> None:
+        pygame.init()
         #set game window
         self.WIDTH = 1440
         self.HEIGHT = 900
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE)
-        self.canvas = pygame.Surface((self.WIDTH, self.HEIGHT))
+
 
         pygame.display.set_caption('Looney Tunes Castle Defence')
         icon = pygame.image.load(os.path.dirname(os.path.dirname(__file__)) + "/assets/misc/icon.png")
         pygame.display.set_icon(icon)
 
         #set variables
+        self.clock = pygame.time.Clock()
         self.running = True
         self.dt = 0
         self.previousTime = 0
         self.FPS = 30
         self.stateStack = []
+        self.inputs = {}
 
-    #loads images
-    def loadImages(self) -> None:
-        self.assetsFolder = os.path.join("assets")
-        self.charactersFolder = os.path.join(self.assetsFolder, "characters")
-        self.mainGameFolder = os.path.join(self.assetsFolder, "main game")
-        self.mainMenuFolder = os.path.dirname(os.path.dirname(__file__)) + "/assets/main menu"
-        self.roadRunnerMinigame = os.path.join(self.assetsFolder, "road runner minigame")
-        self.tweetyPieMinigame = os.path.join(self.assetsFolder, "tweety pie minigame")
-        self.miscFolder = os.path.join(self.assetsFolder, "misc")
-    
+    #initialises states
+    def loadStates(self) -> None:
+        self.mainMenu = mainMenuState.MainMenu(self)
+        self.stateStack.append(self.mainMenu )
+
     #game loop
     def gameLoop(self) -> None:
-        self.loadImages()
+        self.loadStates()
+        self.clock.tick(self.FPS)
         while self.running:
             self.get_dt()
             self.getEvents()
             self.update()
             self.render()
-            print(self.mainMenuFolder)
 
     #gets pygame events and responds to them
     def getEvents(self) -> None:
@@ -50,18 +48,18 @@ class Main():
                 self.running = False
             
     def update(self) -> None:
-        pass
+        self.stateStack[-1].update(self.dt)
 
     #renders game screen
     def render(self) -> None:
-        self.screen.blit(self.canvas, (0,0))
+        self.stateStack[-1].render(self.screen)
+        
 
     #sets delta time value
     def get_dt(self) -> None:
         timeNow = time.time()
         self.dt = timeNow - self.previousTime
         self.previousTime = timeNow
-    
 
 
 if __name__ == "__main__":
